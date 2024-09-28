605a82ed3640b5fd117ce3516dbce62f268b99e9
ParthPidurkar <parthpidurkar667@gmail.com>
2024-09-28 23:06:32 +0530

    basics

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,32 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
-
+import Product from './components/Products';
+import ProductF from './components/ProductF';
 
 class App extends Component {
+
+
 
 
   render() {
     // any processing ....
 
-
     return (
       <div className="App">
 
+        <br/>
+        <Product name="HP Laptop" price="10000" avl="true"> </Product>
+        <Product name="Samsung Laptop" price="20000" avl="false"> </Product>
 
-        Hello React
+      <hr/>
+
+        <ProductF name="F HP Laptop" price="1" avl="true"> </ProductF>
+        <ProductF name="F Samsung Laptop" price="2" avl="false"> </ProductF>
       </div>
     );
-
 
   }
 
 
-
-
 }
 
-
 export default App;
-
-
---
+++ my-app/src/components/Products.js
@@ -0,0 +1,34 @@
+
+import { Component } from 'react';
+
+class Product extends Component {
+     productName = "Samsung Mobile";
+     productPrice=0;
+
+     constructor(props){
+        super(props);
+        this.productName = this.props.name;
+        this.productPrice = this.props.price;
+     }
+
+
+    render() {
+        let x = 10;
+
+        return (
+
+            <div>
+
+            {/* <p> value of x is {x} </p> */}
+            <p> value of productName is {this.productName} </p>
+            <p> value of productPrice is {this.productPrice} </p>
+
+            </div>
+
+        )
+
+    }
+
+}
+
+export default Product;
