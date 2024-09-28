7f0ff39ec1e6d3225280c276deb9194d2bcebc0d
ParthPidurkar <parthpidurkar667@gmail.com>
2024-09-28 21:49:36 +0530

    React Js Bascis

---
+++ my-app/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+
+class App extends Component {
+
+
+  render() {
+    // any processing ....
+
+
+    return (
+      <div className="App">
+
+
+        Hello React
+      </div>
+    );
+
+
+  }
+
+
+
+
+}
+
+
+export default App;
+
+
